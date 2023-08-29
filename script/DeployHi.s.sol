a437f6edab1ff2de034f4b5810791690cb033154
zapaz.eth <alain@kredeum.com>
2023-08-29 21:05:15 +0200

    deploy setup for sepolia, chiado  and gnosis

---
+++ script/DeployHi.s.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {DeployLite} from "forge-deploy-lite/script/DeployLite.sol";
+import {Hi} from "src/Hi.sol";
+
+contract DeployHi is DeployLite {
+    function deployHi() public returns (address hi) {
+        vm.startBroadcast();
+        hi = address(new Hi());
+        vm.stopBroadcast();
+    }
+
+    function run() public virtual {
+        deploy("Hi");
+    }
+}
