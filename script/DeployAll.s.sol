3a6e24e8f30c6fff1026beaf5ee7afc8d695274d
zapaz.eth <alain@kredeum.com>
2023-08-31 13:17:17 +0200

    deploy erc6551 on chiado

---
+++ script/DeployAll.s.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import {DeployERC6551Registry} from "script/deploy/DeployERC6551Registry.s.sol";
+import {DeploySimpleERC6551Account} from "script/deploy/DeploySimpleERC6551Account.s.sol";
+
+contract DeployAll is DeployERC6551Registry, DeploySimpleERC6551Account {
+    function run() public override(DeployERC6551Registry, DeploySimpleERC6551Account) {
+        deploy("ERC6551Registry");
+        deploy("SimpleERC6551Account");
+    }
+}
